f254e45f41efc90c9bcde703551242818aa3d97f
OsamaAlhosni <55885836+OsamaAlhosni@users.noreply.github.com>
2020-12-13 08:01:07 +0200

    test

--- invoices/models.py
+++ invoices/models.py
@@ -22,3 +22,8 @@ class Invoice(models.Model):
     def __str__(self):
         return self.customer_name
         # , self.Invoice_no, self.proid, self.invoice_amount
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=200, null=False, blank=False)
+    beginig_balance = models.DecimalField(max_digits=19,decimal_places=3,null=True,blank=True)
+    is_active = models.BooleanField(default=True)
