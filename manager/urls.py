1a4aee99f205d5e9e508c2014b84a8bfb4d30e90
OsamaAlhosni <55885836+OsamaAlhosni@users.noreply.github.com>
2021-01-05 14:55:58 +0200

    update users

--- manager/urls.py
+++ manager/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.manager_list ,name='manager_list'),
+    path('add_user',views.add_user ,name='add_user'),
+    path('users_list',views.users_list ,name='users_list'),    
+
     
 
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Manager
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
 
 def manager_list(request):
-    managers = Manager.objects.all()
-    return render(request,'manager/manager_list.html',{'managers':managers})
+    if not request.user.is_authenticated:
+     return redirect('mylogin')
 
-def add_manager(request):
-    pass
+    
+    if request.method == 'POST':
+        manager_name = request.POST.get('manager_name')
+        if manager_name =='':
+            return redirect('manager_list')
+        manager =Manager(manager_name= manager_name)
+        manager.save()
+
+    managers = Manager.objects.all()
+    paginator = Paginator(managers, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'manager/manager_list.html',{'page_obj':page_obj})
+
+def add_user(request):
+    if request.method == 'POST':
+        user_name= request.POST.get('emp_no')
+        first_name = request.POST.get('emp_name')
+        email = request.POST.get('emp_email')
+    return render(request,'manager/add_user.html')
+
+def users_list(request):
+    users = User.objects.all()
+    paginator = Paginator(users, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,'manager/users_list.html',{'page_obj':page_obj})
