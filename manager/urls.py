72e8e856663557a5b89a019209a2d5fdbad5cf61
OsamaAlhosni <55885836+OsamaAlhosni@users.noreply.github.com>
2021-01-06 14:09:46 +0200

    udate user

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('',views.manager_list ,name='manager_list'),
     path('add_user',views.add_user ,name='add_user'),
-    path('users_list',views.users_list ,name='users_list'),    
+    path('users_list',views.users_list ,name='users_list'),  
+    path('delete_user/<int:user_id>',views.delete_user, name='delete_user'),
+    path('edit_user/<int:user_id>',views.edit_user, name='edit_user'),    
 
     
 
--- manager/views.py
+++ manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from . models import Manager
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
@@ -22,6 +22,10 @@ def manager_list(request):
     return render(request,'manager/manager_list.html',{'page_obj':page_obj})
 
 def add_user(request):
+
+    if not request.user.is_authenticated:
+       return redirect('mylogin')
+
     if request.method == 'POST':
         user_name= request.POST.get('emp_no')
         first_name = request.POST.get('emp_name')
@@ -37,13 +41,48 @@ def add_user(request):
         user = User.objects.create_user(username=user_name,email=email,password=password1,first_name=first_name)
         
         return redirect('users_list')
-
-    return render(request,'manager/add_user.html')
+    else:
+        return render(request,'manager/add_user.html')
 
 def users_list(request):
+
+    if not request.user.is_authenticated:
+       return redirect('mylogin')
+
     users = User.objects.all()
     paginator = Paginator(users, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     return render(request,'manager/users_list.html',{'page_obj':page_obj})
+
+def delete_user(request,user_id):
+
+     if not request.user.is_authenticated:
+         return redirect('mylogin')
+
+     user = get_object_or_404(User, pk=user_id)
+     user.delete()
+     return redirect('users_list')
+
+def edit_user(request,user_id):
+
+    if not request.user.is_authenticated:
+       return redirect('mylogin')
+
+    user = get_object_or_404(User, pk=user_id)
+
+    if request.method == 'POST':
+        emp_no = request.POST.get('emp_no')  
+        email = request.POST.get('emp_email') 
+        emp_name = request.POST.get('emp_name')
+        if emp_no == '' or email == '' or emp_name == '':
+            err = 'جميع الحقول مطلوبة'
+            return render(request,'manager/edit_user.html',{'user':user,'err':err}) 
+        user.username=emp_no
+        user.first_name= emp_name
+        user.email=email
+        user.save()
+        return redirect('users_list')
+    
+    return render(request,'manager/edit_user.html',{'user':user})
